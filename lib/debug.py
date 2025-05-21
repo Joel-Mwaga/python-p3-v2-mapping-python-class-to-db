e65016650aba29fdb5e6fd1861b43fb6a683cf67
Joel Mwaga <mwagahjoel14@gmail.com>
2025-05-21 15:32:21 +0300

    learnt about mapping

--- lib/debug.py
+++ lib/debug.py
@@ -4,6 +4,62 @@
 from __init__ import CONN, CURSOR
 from department import Department
 
+class Department:
+    def __init__(self, name, location, id=None):
+        self.id = id
+        self.name = name
+        self.location = location
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS departments (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                location TEXT
+            )
+        """
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        sql = "DROP TABLE IF EXISTS departments"
+        CURSOR.execute(sql)
+        CONN.commit()
+
+    def save(self):
+        if self.id is None:
+            sql = """
+                INSERT INTO departments (name, location)
+                VALUES (?, ?)
+            """
+            CURSOR.execute(sql, (self.name, self.location))
+            CONN.commit()
+            self.id = CURSOR.lastrowid
+        else:
+            self.update()
+
+    @classmethod
+    def create(cls, name, location):
+        department = cls(name, location)
+        department.save()
+        return department
+
+    def update(self):
+        sql = """
+            UPDATE departments
+            SET name = ?, location = ?
+            WHERE id = ?
+        """
+        CURSOR.execute(sql, (self.name, self.location, self.id))
+        CONN.commit()
+
+    def delete(self):
+        sql = "DELETE FROM departments WHERE id = ?"
+        CURSOR.execute(sql, (self.id,))
+        CONN.commit()
+
 import ipdb
 
 ipdb.set_trace()
